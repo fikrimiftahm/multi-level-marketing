34832da362c376e2063bd40649de865d29a5f41b
Fikri M Maulana <fikrimiftahm@gmail.com>
2022-10-20 21:18:30 +0700

    Remove db name

--- app/Repository/Eloquent/MemberRepository.php
+++ app/Repository/Eloquent/MemberRepository.php
@@ -78,15 +78,15 @@ class MemberRepository implements MemberRepositoryInterface
             $query = '
             with members as (
                 select m.id member_id, m.name member_name, lm.leader_id 
-                from practice.members m
-                left join practice.leaders_members lm 
+                from members m
+                left join leaders_members lm 
                 on m.id = lm.member_id
-                left join practice.leaders l 
+                left join leaders l 
                 on m.id = l.member_id 
             ), mlm as (
                 select m.*, me.name leader_name
                 from members m
-                left join practice.members me 
+                left join members me 
                 on m.leader_id = me.id
                 order by 1 asc, 3 asc
             )
@@ -107,16 +107,16 @@ class MemberRepository implements MemberRepositoryInterface
             $query = "
             with leaders as (
                 select m.id leader_id, m.name leader_name, lm.member_id 
-                from practice.leaders l
-                left join practice.leaders_members lm 
+                from leaders l
+                left join leaders_members lm 
                 on l.member_id = lm.leader_id
-                left join practice.members m
+                left join members m
                 on m.id = lm.leader_id 
                 where m.name = '" . $name . "'
             ), mlm as (
                 select l.*, me.name member_name
                 from leaders l
-                left join practice.members me 
+                left join members me 
                 on l.member_id = me.id
                 order by 1 asc, 3 asc
             ), smr as (
@@ -142,15 +142,15 @@ class MemberRepository implements MemberRepositoryInterface
             $query = "
             with leaders as (
                 select m.id leader_id, m.name leader_name, lm.member_id 
-                from practice.leaders l
-                left join practice.leaders_members lm 
+                from leaders l
+                left join leaders_members lm 
                 on l.member_id = lm.leader_id
-                left join practice.members m
+                left join members m
                 on m.id = lm.leader_id 
             ), mlm as (
                 select l.*, me.name member_name
                 from leaders l
-                left join practice.members me 
+                left join members me 
                 on l.member_id = me.id
                 order by 1 asc, 3 asc
             ), smr as (
